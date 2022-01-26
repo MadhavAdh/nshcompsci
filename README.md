7c3d331a0b72328bd72f116261114027637a83a7
Madhav Adhikari <98475360+MadhavAdh@users.noreply.github.com>
2022-01-26 16:02:33 +0000

    add echo.cpp

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # NHS Computer Science
 This is my cod repository for NHS Computer Science!
 
-This is my first replit edit>
+This is my first replit edit
---
+++ echo/echo.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+  std::string intput;
+  std::getline(std::cin, intput);
+  std::cout << intput << "\n";
+}
